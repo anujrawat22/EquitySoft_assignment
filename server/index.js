57b79fa0b018d4e98ededdde5c9360244a2c39a9
anujrawat22 <anuj22rawat20@gmail.com>
2023-08-21 20:53:15 +0530

    user login signup completed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const { Connection } = require("./config/db")
 require("dotenv").config()
+const cors = require("cors")
+const { UserRouter } = require("./routes/UserRoute")
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(express.json())
+app.use(cors())
 
+app.use("/api/users",UserRouter)
 
 
 app.listen(port, async () => {
