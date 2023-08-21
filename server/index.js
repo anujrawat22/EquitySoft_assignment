45605dbb53941bf1492c103fd2e2f0746e520de5
anujrawat22 <anuj22rawat20@gmail.com>
2023-08-21 18:20:35 +0530

    server setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { Connection } = require("./config/db")
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT || 8080
+
+
+app.listen(port, async () => {
+    try {
+        await Connection;
+        console.log('Connected to DB');
+        console.log(`Listening on PORT - ${port}`);
+    } catch (error) {
+console.log(error);
+    }
+})
